8770dad1a22b53a5d391cbd07f1434c29d664069
joaofelix27 <joaofelix27@gmail.com>
2022-09-23 16:11:28 -0300

    Adding front end folder

--- tests/factories/deleteAll.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from "../../src/database";
-
-
-export async function deleteAllData() {
-    await prisma.$transaction([
-      prisma.$executeRaw`TRUNCATE TABLE recommendations`
-    ]);
-  }
